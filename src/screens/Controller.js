c4a1da659cd6b9d3f4af665801355d4ddb39472a
gayathrichelluri <gayathri4040#gmail.com>
2021-09-13 21:43:50 +1000

    Header - Logo, Login/Logout, Book Show buttons

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -4,6 +4,7 @@ import Details from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
+import Header from "../common/header/Header";
 
 const Controller = () => {
     const baseUrl = "/api/v1/";
@@ -11,23 +12,25 @@ const Controller = () => {
     return (
         <Router>
             <div className="main-container">
-                <Route
-                    exact
-                    path="/"
-                    render={(props) => <Home {...props} baseUrl={baseUrl} />}
-                />
-                <Route
-                    path="/movie/:id"
-                    render={(props) => <Details {...props} baseUrl={baseUrl} />}
-                />
-                <Route
-                    path="/bookshow/:id"
-                    render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
-                />
-                <Route
-                    path="/confirm/:id"
-                    render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-                />
+                <Header>
+                    <Route
+                        exact
+                        path="/"
+                        render={(props) => <Home {...props} baseUrl={baseUrl} />}
+                    />
+                    <Route
+                        path="/movie/:id"
+                        render={(props) => <Details {...props} baseUrl={baseUrl} />}
+                    />
+                    <Route
+                        path="/bookshow/:id"
+                        render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
+                    />
+                    <Route
+                        path="/confirm/:id"
+                        render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
+                    />
+                </Header>
             </div>
         </Router>
     );
