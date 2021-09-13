60819eb05ccc6738d45fe0f29adf5d208b5a24cc
gayathrichelluri <gayathri4040#gmail.com>
2021-09-14 00:31:10 +1000

    Implemented Modal

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 import logoSVG from '../../assets/logo.svg';
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import Modal from '../modal/Modal';
+import Register from "../../screens/register/Register";
+import Login from "../../screens/login/Login";
 
 export const Header = () => {
-    const isLoggedIn = false;
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    const onLoginClick = () => {
+        setLoggedIn(!loggedIn);
+    }
+
+    const onBookShowClick = () => {
+        !loggedIn ? setShowModal(!showModal) : console.log('Should show Book Show screen');
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
 
     return (
-        <div className='header'>
-            <img className='logo' src={logoSVG} alt="React Logo" />
-            <div className='buttons'>
-                <Button className='button' variant='contained' color='primary'>BOOK SHOW</Button>
-                <Button className='button' variant='contained'>{isLoggedIn ? 'LOGOUT' : 'LOGIN'}</Button>
+        <React.Fragment>
+            <div className='header'>
+                <img className='logo' src={logoSVG} alt="React Logo" />
+                <div className='buttons'>
+                    <Button className='button' variant='contained' color='primary' onClick={onBookShowClick}>BOOK SHOW</Button>
+                    <Button className='button' variant='contained' onClick={onLoginClick}>{loggedIn ? 'LOGOUT' : 'LOGIN'}</Button>
+                </div>
             </div>
-        </div>
+            {showModal &&
+            <Modal
+                open={showModal}
+                close={closeModal}
+                tabs={['Login', 'Register']}
+                tabActions={{
+                    Login: <Login />,
+                    Register: <Register />
+                }}
+            />
+            }
+        </React.Fragment>
     );
 }
 
 export default Header;
 
 /* --------------------- TODO ---------------------
-
-LOGIN/REGISTER MODAL
+Access token for - 'loggedIn' state
 
 BOOK SHOW button -
 1. This button should always be displayed in the header when a user clicks on a released movie, whether they are logged in or not.
---
+++ src/common/modal/Modal.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {Tab, Tabs} from "@material-ui/core";
+import ReactModal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+export const Modal = ({open, close, tabs, tabActions}) => {
+    const [tab, setTab] = useState({value: 0, action: tabActions['Login']});
+
+    const onTabChange = (event, value) => {
+        setTab({value: value, action: tabActions[tabs[value]]});
+    }
+
+    return (
+        <ReactModal
+            isOpen={open}
+            onRequestClose={close}
+            shouldCloseOnOverlayClick={true}
+            style={customStyles}
+        >
+                <Tabs
+                    value={tab.value}
+                    onChange={onTabChange}
+                    indicatorColor="primary"
+                    variant="fullWidth"
+                    TabIndicatorProps={{
+                        style: {
+                            backgroundColor: 'red',
+                        }
+                    }}
+                >
+                    {tabs.map((tab, idx) => <Tab key={idx} label={tab} /> )}
+                </Tabs>
+                {tabActions[tabs[tab.value]]}
+        </ReactModal>
+    );
+}
+
+export default Modal;
